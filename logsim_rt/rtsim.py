66b87df492a6202c75f8f8271a3f4a8653a5e1ed
Andy <andymcdonald1985@gmail.com>
2022-06-26 17:04:41 +0100

    initial library setup

---
+++ logsim_rt/rtsim.py
@@ -0,0 +1,10 @@
+class rtsim:
+    def __init__(self) -> None:
+        pass
+    
+    @classmethod
+    def data_from_csv(cls):
+        raise NotImplemented
+
+    def run(self):
+        raise NotImplemented
---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="logsim_rt",
+    version="0.1.0",
+    url="https://github.com/andymcdgeo/real_time_log_data_simulator",
+    author="Andy McDonald",
+    description="Experiment to create real time simulations of log data",
+    long_description=open('README.md').read(),
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
