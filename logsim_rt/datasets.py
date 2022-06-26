5cdbc872816c8699873f452e029d14d9a5262624
Andy <andymcdonald1985@gmail.com>
2022-06-26 17:04:29 +0100

    Initial layout of library

---
+++ logsim_rt/datasets.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def load_volve_f4_dataset() -> pd.DataFrame:
+    raise NotImplemented
+
+def load_volve_f5_dataset() -> pd.DataFrame:
+    raise NotImplemented
+
+def load_volve_drilling_dataset() -> pd.DataFrame:
+    raise NotImplemented
