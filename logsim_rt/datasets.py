7723db7a3656b4cb1acebb28bbee983c42352d18
Andy <andymcdonald1985@gmail.com>
2022-06-28 15:52:52 +0100

    Volve 19 can now be loaded

--- logsim_rt/datasets.py
+++ logsim_rt/datasets.py
@@ -1,10 +1,35 @@
 import pandas as pd
+import pkg_resources
 
-def load_volve_f4_dataset() -> pd.DataFrame:
-    raise NotImplemented
+def load_volve_19_dataset() -> pd.DataFrame:
+    """Return a dataframe of well 15/9-19 from the Equinor Volve dataset.
+
+    Contains the following fields:
+        DEPTH
+        CALI
+        DT
+        DTS
+        GR
+        NPHI
+        PHIE
+        RHOB
+        RT
+        TEMP
+
+    Returns
+    -------
+    pd.DataFrame
+        Returns a pandas dataframe object containing data from well 15/9-19 of the Volve field
+    """
+    stream = pkg_resources.resource_stream(__name__, 'data/EQR_Volve_Well_15_9-19.csv')
+    return pd.read_csv(stream, encoding='latin-1')
 
 def load_volve_f5_dataset() -> pd.DataFrame:
     raise NotImplemented
 
 def load_volve_drilling_dataset() -> pd.DataFrame:
     raise NotImplemented
+
+if __name__ == "__main__":
+    df = load_volve_19_dataset()
+    print(df.head())
